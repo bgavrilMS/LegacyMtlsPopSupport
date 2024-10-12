80ad1e7f81fd061729976c2ecfa796adefdc773b
Bogdan Gavril <bogavril@microsoft.com>
2024-10-11 17:15:59 -0700

    Remove appsettings

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Azure.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Identity.Abstractions;
@@ -6,6 +7,7 @@ using Microsoft.Identity.Client;
 using Microsoft.Identity.Client.Extensibility;
 using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.S2S.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Security.Cryptography;
@@ -14,6 +16,9 @@ using System.Text;
 
 internal class Program
 {
+    const string Authority = "https://login.microsoftonline.com/f645ad92-e38d-4d1a-b510-d1b09a74a8ca";
+    const string ClientId = "88f91eac-c606-4c67-a0e2-a5e8a186854f";
+
     private static async Task Main(string[] args)
     {
         // Get the Token acquirer factory instance. By default it reads an appsettings.json
@@ -27,31 +32,30 @@ internal class Program
                                               .AddConsole());
 
         var sp = tokenAcquirerFactory.Build();
-        var ta = tokenAcquirerFactory.GetTokenAcquirer();
+        
+        CertificateDescription credential = CertificateDescription.FromStoreWithDistinguishedName("CN=LabAuth.MSIDLab.com");
+        DefaultCertificateLoader.LoadFirstCertificate([credential]);
+        X509Certificate2 certificate = credential.Certificate!;
 
-        X509Certificate2 certificate = GetCertificateFromConfig(sp);
+        ITokenAcquirer tokenAcquirer = tokenAcquirerFactory.GetTokenAcquirer(new MicrosoftIdentityApplicationOptions()
+        {
+            Authority = Authority, 
+            ClientId = ClientId,
+            ClientCredentials = [credential], 
+            SendX5C = true
+        });
 
         var thumbprint = Base64UrlEncoder.Encode(certificate.GetCertHash(HashAlgorithmName.SHA256));
 
         var reqCnf = $@"{{""kty"":""RSA"",""x5t#S256"":""{thumbprint}"",""kid"":""{thumbprint}""}}";
 
-        var token = await ta.GetTokenForAppAsync(
+        AcquireTokenResult token = await tokenAcquirer.GetTokenForAppAsync(
             "https://vault.azure.net/.default",
             new AcquireTokenOptions() { 
                 PopPublicKey = thumbprint,
                 PopClaim = reqCnf });
     }
 
-    /// <summary>
-    /// This just uses Id.Web to get the cert from config. You can use any method to get the cert.
-    /// </summary>
-    private static X509Certificate2 GetCertificateFromConfig(IServiceProvider sp)
-    {
-        var appOptions = sp.GetRequiredService<IOptions<MicrosoftIdentityApplicationOptions>>();
-        CertificateDescription credentialDescription = new CertificateDescription(appOptions.Value.ClientCredentials!.First());
-        DefaultCertificateLoader.LoadFirstCertificate(new[] { credentialDescription });
-        X509Certificate2 certificate = credentialDescription.Certificate!;
-        return certificate;
-    }
+   
 
 }
